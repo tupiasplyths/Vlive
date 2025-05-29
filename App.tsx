987bcbe00d377b08a0443cd80a867dfd02544dd2
Nguyễn Quang Dương <furygod7@gmail.com>
2025-05-29 11:48:58 +0700

    try fix stuff

--- App.tsx
+++ App.tsx
@@ -1,3 +1,5 @@
+import 'react-native-gesture-handler';
+import React from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 // import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native'
@@ -15,13 +17,11 @@ export default function App() {
 	
 
 	return (
-	<GestureHandlerRootView>
+	<GestureHandlerRootView style={{ flex: 1 }}>
 		<NavigationContainer>
 			<Drawer.Navigator>
 				<Drawer.Screen name="Home" component={LiveChannels} />
-				<Drawer.Screen name="Add Channel" component={Input} />
 				<Drawer.Screen name="Search Channel" component={Search} />
-				<Drawer.Screen name="Flush Channel List" component={Flush} />
 			</Drawer.Navigator>
 		</NavigationContainer>
 		<Toast />
