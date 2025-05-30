bd39e666f148a3320f248f97e60e2b62cfe90d8a
Nguyễn Quang Dương <106746129+NightSky17203@users.noreply.github.com>
2025-05-30 16:26:13 +0700

    app_version_1.0.0

--- utils/Redirect.ts
+++ utils/Redirect.ts
@@ -3,22 +3,10 @@ import { Linking, Alert, Platform } from 'react-native';
 const openYouTubeVideo = async (videoId: string) => {
 
     const appURL = `youtube://watch?v=${videoId}`;
-
-  // const webURL = `https://www.youtube.com/watch?v=${videoId}`;
-
-  try {
-    const canOpenAppURL = await Linking.canOpenURL(appURL);
-    if (canOpenAppURL) {
-        console.log(appURL);
-      await Linking.openURL(appURL);
-    } else {
-      const canOpenWebURL = await Linking.canOpenURL(webURL);
-      if (canOpenWebURL) {
-        await Linking.openURL(webURL);
-      } else {
-        Alert.alert("Lỗi", "Me can't open Youtube app for you");
-      }
-    }
+    try {
+      await Linking.openURL(`vnd.youtube:${videoId}`);
+      console.log('Successfully opened with YouTube app');
+      return;
   } catch (error) {
     console.error("Lỗi khi mở YouTube:", error);
     Alert.alert("Error");
